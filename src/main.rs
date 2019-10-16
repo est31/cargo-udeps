5c54c05848caf3e9053da6929597c2f5ddc55e8d
Ryo Yamashita <qryxip@gmail.com>
2019-10-17 02:13:45 +0900

    Use `cargo::exit_with_error`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
 
-use cargo::CliResult;
+use cargo::core::shell::Shell;
 
-fn main() -> CliResult {
-	let mut config = cargo::Config::default()?;
-	cargo_udeps::run(env::args_os(), &mut config)
+fn main() {
+	let mut config = cargo::Config::default()
+		.unwrap_or_else(|e| cargo::exit_with_error(e.into(), &mut Shell::new()));
+	if let Err(err) = cargo_udeps::run(env::args_os(), &mut config) {
+		cargo::exit_with_error(err, &mut config.shell());
+	}
 }
