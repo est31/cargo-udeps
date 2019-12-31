5a5c46d9dd95386a5dd195c4670a1a16b4622565
Ryo Yamashita <qryxip@gmail.com>
2019-12-31 17:59:53 +0900

    Not all dependencies are libraries

--- tests/non-lib-build-dep.rs
+++ tests/non-lib-build-dep.rs
@@ -6,11 +6,13 @@ use pretty_assertions::assert_eq;
 use crate::runner::Runner;
 
 static CARGO_TOML :&str = r#"[workspace]
+
 [package]
-name = "unused_byteorder"
-version = "0.0.1"
-[dependencies]
-byteorder = "1.0.0"
+name = "non_lib_build_dep"
+version = "0.0.0"
+
+[build-dependencies]
+diffr = "=0.1.3"
 "#;
 
 static LIB_RS :&str = "";
@@ -18,7 +20,7 @@ static LIB_RS :&str = "";
 #[test]
 fn without_all_targets() -> CargoResult<()> {
 	let (code, stdout_masked) =
-		Runner::new("cargo_udeps_test_unused_byteorder_without_all_targets")?
+		Runner::new("cargo_udeps_test_non_lib_build_dep_without_all_targets")?
 			.cargo_toml(CARGO_TOML)?
 			.dir("./src")?
 			.file("./src/lib.rs", LIB_RS)?
@@ -26,11 +28,13 @@ fn without_all_targets() -> CargoResult<()> {
 	assert_eq!(1, code);
 	assert_eq!(
 		r#"unused dependencies:
-`unused_byteorder v0.0.1 (██████████)`
-└─── (dev-)dependencies
-     └─── "byteorder"
+`non_lib_build_dep v0.0.0 (██████████)`
+└─── build-dependencies
+     └─── "diffr"
 Note: These dependencies might be used by other targets.
       To find dependencies that are not used by any target, enable `--all-targets`.
+Note: Some dependencies are non-library packages.
+      `cargo-udeps` regards them as unused.
 Note: They might be false-positive.
       For example, `cargo-udeps` cannot detect usage of crates that are only used in doc-tests.
 "#,
@@ -43,7 +47,7 @@ Note: They might be false-positive.
 #[test]
 fn with_all_targets() -> CargoResult<()> {
 	let (code, stdout_masked) =
-		Runner::new("cargo_udeps_test_unused_byteorder_with_all_targets")?
+		Runner::new("cargo_udeps_test_non_lib_build_dep_with_all_targets")?
 			.cargo_toml(CARGO_TOML)?
 			.dir("./src")?
 			.file("./src/lib.rs", LIB_RS)?
@@ -52,9 +56,11 @@ fn with_all_targets() -> CargoResult<()> {
 	assert_eq!(1, code);
 	assert_eq!(
 		r#"unused dependencies:
-`unused_byteorder v0.0.1 (██████████)`
-└─── (dev-)dependencies
-     └─── "byteorder"
+`non_lib_build_dep v0.0.0 (██████████)`
+└─── build-dependencies
+     └─── "diffr"
+Note: Some dependencies are non-library packages.
+      `cargo-udeps` regards them as unused.
 Note: They might be false-positive.
       For example, `cargo-udeps` cannot detect usage of crates that are only used in doc-tests.
 "#,
