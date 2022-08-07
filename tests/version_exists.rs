6ae903ba72e746a9855dd9dd74dbaea58d880e2e
Ricky Hosfelt <ricky_github@hosfe.lt>
2022-08-06 19:34:55 -0700

    Add back version information and add test to ensure it stays

---
+++ tests/version_exists.rs
@@ -0,0 +1,37 @@
+mod runner;
+
+use anyhow::anyhow;
+use cargo::CargoResult;
+use pretty_assertions::assert_eq;
+
+use crate::runner::Runner;
+
+use std::{fmt, error};
+
+#[derive(Debug)]
+enum VersionError {
+    Stdout
+}
+
+impl error::Error for VersionError {}
+
+// Implement `Display` for `version_error` required for `std::error::Error`.
+impl fmt::Display for VersionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error: expect the command to output version information from `stderr`")
+    }
+}
+
+
+#[test]
+fn check_version_exists() -> CargoResult<()> {
+	match Runner::new("cargo_udeps_test_check_version_information")?.arg("--version").run() {
+            Ok(_) => Err(anyhow!(VersionError::Stdout)),
+            Err(error) => {
+	assert_eq!(
+		format!("cargo-udeps {}\n", env!("CARGO_PKG_VERSION")),
+		error.to_string(),);
+        Ok(())
+        }
+    }
+}
